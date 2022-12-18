3e67ed7dd0816ceeafd395494e61442f37c8cb4d
remus draica <remus.draica@gmail.com>
2022-12-18 14:07:49 +0200

    ui part

---
+++ OBSERVATIONS.md
@@ -0,0 +1,5 @@
+## ui
+
+Categories are buttons. There is a message with the category clicked by user. The drawback is that there is no visual distinction when mouse is moved over the 'category' button, the 'cursor' mouse pointer being displayed for both.
+
+Because cards have a min width, a max width is also necessary. Otherwise there is only one column with cards.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,20 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { ToastrModule } from 'ngx-toastr';
+import { MatCardModule } from '@angular/material/card';
+
 
 @NgModule({
-  imports: [BrowserModule, CommonModule],
+  imports: [
+    BrowserModule, 
+    BrowserAnimationsModule,
+    CommonModule,
+    MatCardModule,
+    ToastrModule.forRoot()
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
