2ac903fec0de7dce2a203fd3f73bf6b70754253e
Diana Dima <diana.dima.1995@gmail.com>
2022-12-19 09:23:56 +0200

    completed task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,24 @@ import { ProductService } from './products/product.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private readonly productService: ProductService) {}
-
   readonly products$: Observable<Page<Product>> = this.productService.get(0);
+  pageNo: number = 0;
+  products: Product[] = [];
+  hasMore: boolean = true;
+
+  constructor(private readonly productService: ProductService) {
+    this.loadProducts();
+  }
+
+  loadMore(){
+    this.pageNo++;
+    this.loadProducts();
+  }
+
+  loadProducts(){
+    this.productService.get(this.pageNo).subscribe((response:Page<Product>)=>{
+      this.products = [...this.products, ...response.content];
+      this.hasMore = response.more;
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   imports: [BrowserModule, CommonModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, CardComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
