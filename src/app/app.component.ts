cbdcea868a818c4a7f6af53468680234778a230f
Cristina Purice <cristina.purice@odeen.ro>
2023-03-07 17:44:57 +0200

    Cristina Purice - test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, catchError, merge, Observable, of, scan, switchMap, tap } from 'rxjs';
 import { Page } from './products/page';
 import { Product } from './products/product';
 import { ProductService } from './products/product.service';
@@ -12,5 +12,44 @@ import { ProductService } from './products/product.service';
 export class AppComponent {
   constructor(private readonly productService: ProductService) {}
 
-  readonly products$: Observable<Page<Product>> = this.productService.get(0);
+  // readonly products$: Observable<Page<Product>> = this.productService.get(0);
+
+  public currentPage$ = new BehaviorSubject<number>(1);
+  private _initList: Page<Product> = { more: true,  content: []};
+  private initialList$ = of(this._initList);
+
+  public productUpdate$ = this.currentPage$.pipe(
+    tap(x=>{
+      this.changeLoading();
+    }),
+    switchMap((currentPage)=> this.productService.get(currentPage))
+      
+  );
+
+  public loading$: boolean = false;
+  private changeLoading = ():boolean => this.loading$ = !this.loading$;
+  // Merge the streams
+  public products$ = merge(
+      this.productUpdate$,
+      this.initialList$
+    ).pipe(
+        // this.changeLoading(),
+        scan((acc, value) => { 
+          this.changeLoading();
+          return {more: acc.more && value.more, content: acc.content.concat(value.content)}}),
+        
+      );
+
+  public loadMore = ():void  => {
+    this.currentPage$.next(this.currentPage$.value + 1); 
+  }
+
+  public refresh(): void {
+    window.location.reload();
+  }
+
+  public goToLink(url: string): void {
+    window.open(url, "_blank");
+}
+
 }
