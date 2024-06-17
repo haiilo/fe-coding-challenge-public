2808ad375f071c7e2e8c6b77f6d4262874bd6a5f
Tudor Merlas <tmerlas@reshapedigital.io>
2024-06-17 09:24:40 +0300

    Extracted the products grid product into a separate component to further separate concerns.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ProductsGridComponent } from './products/components/products-grid/products-grid.component';
+import { ProductComponent } from './products/components/product/product.component';
 
 @NgModule({
   imports: [BrowserModule, CommonModule],
-  declarations: [AppComponent, ProductsGridComponent],
+  declarations: [AppComponent, ProductsGridComponent, ProductComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
