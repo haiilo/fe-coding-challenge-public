f6ea0c8d027a8f1ccb14cbf9f679e26d7080c1f5
Tudor Merlas <tmerlas@reshapedigital.io>
2024-06-19 08:45:47 +0300

    Extracted all the colors in the app into a colors.scss. Moved products init to ngOnInit for easier testability. Filled in more unit tests.

--- src/app/products/components/products-grid/products-grid.component.spec.ts
+++ src/app/products/components/products-grid/products-grid.component.spec.ts
@@ -1,7 +1,7 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
-import {delay, of} from "rxjs";
+import {ComponentFixture, discardPeriodicTasks, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {delay, of, throwError} from "rxjs";
+import {ProductComponent} from "src/app/products/components/product/product.component";
 import {ProductService} from "src/app/products/services/product.service";
-
 import {ProductsGridComponent} from './products-grid.component';
 
 describe('ProductsGridComponent', () => {
@@ -9,41 +9,84 @@ describe('ProductsGridComponent', () => {
   let fixture: ComponentFixture<ProductsGridComponent>;
   let mockProductService: any;
 
-  beforeEach(async () => {
+  beforeEach(fakeAsync(() => {
     mockProductService = jasmine.createSpyObj('ProductService', ['get']);
-    mockProductService.get.and.callFake((page: number) => {
-      const products = { more: page < 3, content: [{ id: page, title: 'Product ' + page }] };
-      return of(products).pipe(delay(100));
-    });
+    mockProductService.get.and.returnValue(of({ more: true, content: [{ id: 1, title: 'Product 1' }] }).pipe(delay(100)));
 
-    await TestBed.configureTestingModule({
-      declarations: [ ProductsGridComponent ],
-      providers: [
-        { provide: ProductService, useValue: mockProductService }
-      ]
-    })
-      .compileComponents();
+    TestBed.configureTestingModule({
+      declarations: [ProductsGridComponent, ProductComponent],
+      providers: [{ provide: ProductService, useValue: mockProductService }]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ProductsGridComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+    tick(100);
+  }));
+
+  function loadAndWaitForPage() {
+    component.loadMore();
+    tick(100);
+    fixture.detectChanges();
+  }
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should initialize with an empty product list and load products on demand', () => {
-    const testProducts = [{ id: 1, title: 'Test Product', image: '', url: '', description: '', categories: [] }];
-    mockProductService.get.and.returnValue(of({ more: true, content: testProducts }));
+  it('should disable the "Load more" button when canLoadMore is false', fakeAsync(() => {
+    component.canLoadMore$.next(false);
+    fixture.detectChanges();
+    tick();
 
+    const button = fixture.nativeElement.querySelector('.more');
+    expect(button.disabled).toBeTrue();
+  }));
+
+  it('should display an error message when a product load fails', fakeAsync(() => {
+    mockProductService.get.and.returnValue(throwError(() => new Error('Failed to load products')));
+    loadAndWaitForPage();
+
+    const toast = fixture.nativeElement.querySelector('.toasts .toast.danger');
+    expect(toast).toBeTruthy();
+    expect(toast.textContent).toContain('Something went kaput. Please try again later.');
+  }));
+
+  it('should load multiple pages of products and update the UI', fakeAsync(() => {
+    let callCount = 0;
+
+    mockProductService.get.and.callFake(() => of({
+      more: callCount++ < 1,
+      content: [{ id: callCount, title: `Product ${callCount}` }]
+    }).pipe(delay(100)));
+
+    loadAndWaitForPage(); // Load first page
+    loadAndWaitForPage(); // Load second page
+
+    const products = fixture.nativeElement.querySelectorAll('app-product')
+
+    expect(products.length).toBe(3);
+    expect(products[2].querySelector('h3').textContent).toEqual('Product 2');
+  }));
+
+  it('should have a disabled more products button when "more" is false', fakeAsync(() => {
+    mockProductService.get.and.returnValue(of({ more: false, content: [{ id: 4, title: 'Product 4' }] }).pipe(delay(100)));
+    loadAndWaitForPage();
+
+    component.canLoadMore$.subscribe(canLoadMore => {
+      expect(canLoadMore).toBeFalse();
+    });
+
+    const button = fixture.nativeElement.querySelector('.more');
+    expect(button.disabled).toBeTrue();
+  }));
+
+  it('should show a spinner when loading products', fakeAsync(() => {
     component.loadMore();
     fixture.detectChanges();
 
-    component.products$.subscribe(products => {
-      expect(products.length).toBe(1);
-      expect(products[0].title).toEqual('Test Product');
-    });
-  });
-
+    const spinner = fixture.nativeElement.querySelector('.spinner');
+    expect(spinner).toBeTruthy();
+    discardPeriodicTasks();
+  }));
 });
--- src/app/products/components/products-grid/products-grid.component.ts
+++ src/app/products/components/products-grid/products-grid.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnDestroy} from '@angular/core';
-import {BehaviorSubject, catchError, concatMap, map, of, scan, Subject, takeUntil, tap} from "rxjs";
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {BehaviorSubject, catchError, concatMap, map, Observable, of, scan, Subject, takeUntil, tap} from "rxjs";
 import {Page} from "src/app/products/interfaces/page.interface";
 import {Product} from "src/app/products/interfaces/product.interface";
 import {ProductService} from "src/app/products/services/product.service";
@@ -9,39 +9,42 @@ import {ProductService} from "src/app/products/services/product.service";
   templateUrl: './products-grid.component.html',
   styleUrls: ['./products-grid.component.scss']
 })
-export class ProductsGridComponent implements OnDestroy {
+export class ProductsGridComponent implements OnInit, OnDestroy {
 
   private readonly destroyed$ = new Subject<void>()
 
   public ongoingRequests = 0;  // Normally, I'd use an ongoing requests interceptor and service + a spinner component to handle this, but we're not making requests in this example
-  public errorState = false;  // Normally, I'd use an error interceptor and a toast component and service to show errors in a user-friendly way, but we're not making requests in this example
-  public canLoadMore = true;
-
+  public errorState$ = new BehaviorSubject(false);  // Normally, I'd use an error interceptor and a toast component and service to show errors in a user-friendly way, but we're not making requests in this example
+  public canLoadMore$ = new BehaviorSubject(true);
   public page$: BehaviorSubject<number> = new BehaviorSubject(0);
-  public readonly products$ = this.page$.pipe(
-    takeUntil(this.destroyed$),
-    tap(() => {
-      this.errorState = false;
-      this.ongoingRequests++;
-    }),
-    concatMap((page: number) => {
-      return this.productService.get(page).pipe(
-        tap({ complete: () => this.ongoingRequests-- }),
-        catchError(() => {
-          this.ongoingRequests--;
-          this.errorState = true;
-          return of({ more: this.canLoadMore, content: [] } as Page<Product>);
-        })
-      )
-    }),
-    tap((page: Page<Product>) => this.canLoadMore = page.more),
-    map((page: Page<Product>) => page.content),
-    scan((acc, products: Product[]) => acc.concat(products), [] as Product[]),
-  );
+  public products$!: Observable<Product[]>;
 
   public constructor(
     private readonly productService: ProductService
   ) {
+  }
+
+  public ngOnInit() {
+    this.products$ = this.page$.pipe(
+      takeUntil(this.destroyed$),
+      tap(() => {
+        this.errorState$.next(false);
+        this.ongoingRequests++;
+      }),
+      concatMap((page: number) => {
+        return this.productService.get(page).pipe(
+          tap({ complete: () => this.ongoingRequests-- }),
+          catchError(() => {
+            this.ongoingRequests--;
+            this.errorState$.next(true);
+            return of({ more: this.canLoadMore$.value, content: [] } as Page<Product>);
+          })
+        )
+      }),
+      tap((page: Page<Product>) => this.canLoadMore$.next(page.more)),
+      map((page: Page<Product>) => page.content),
+      scan((acc, products: Product[]) => acc.concat(products), [] as Product[]),
+    );
   }
 
   public ngOnDestroy() {
