da443d5b1023bd7baefb766436dc5363853371ab
iancu_robert <robert.iancu@zupria.com>
2024-07-25 20:52:01 +0300

    Implement product navigation url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [BrowserModule, CommonModule],
+  imports: [BrowserModule, CommonModule, RouterModule.forRoot([])],
   declarations: [AppComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
