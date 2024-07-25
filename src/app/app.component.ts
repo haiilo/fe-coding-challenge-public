0606366ea3c297e3f67991fbc6f09e75f7934293
iancu_robert <robert.iancu@zupria.com>
2024-07-25 23:20:40 +0300

    Implement AppState service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
-import { Page } from './products/page';
-import { Product } from './products/product';
-import { ProductService } from './products/product.service';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { AppState } from './app.state';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  constructor(private readonly productService: ProductService) {}
+export class AppComponent implements OnInit {
+  readonly isLoading$ = this.state.isLoading$;
+  readonly canLoadMore$ = this.state.canLoadMore$;
+  readonly products$ = this.state.products$;
 
-  readonly products$: Observable<Page<Product>> = this.productService.get(0);
+  private readonly destroyRef$ = inject(DestroyRef);
+
+  constructor(private readonly state: AppState) {}
+
+  ngOnInit(): void {
+    this.state.getProducts().pipe(takeUntilDestroyed(this.destroyRef$)).subscribe();
+  }
+
+  loadMoreProducts() {
+    this.state.loadMoreProducts().pipe(takeUntilDestroyed(this.destroyRef$)).subscribe();
+  }
 }
