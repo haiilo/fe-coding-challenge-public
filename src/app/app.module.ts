5bb04e4cb05b4ac1a37de77c07a6918014faedf3
iancu_robert <robert.iancu@zupria.com>
2024-07-26 13:06:38 +0300

    Refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
+import { PageLoaderComponent, ProductComponent } from './components';
 
 @NgModule({
   imports: [BrowserModule, CommonModule, RouterModule.forRoot([])],
-  declarations: [AppComponent],
+  declarations: [AppComponent, PageLoaderComponent, ProductComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
